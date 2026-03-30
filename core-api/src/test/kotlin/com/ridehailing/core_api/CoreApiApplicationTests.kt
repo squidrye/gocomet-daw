90645f121302bdc4b5bd158fd8bc239c3db6e5eb
mihir_equirus <mihir@equirus.tech>
2026-03-30 19:51:35 +0530

    chore: basic structure setup

---
+++ core-api/src/test/kotlin/com/ridehailing/core_api/CoreApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ridehailing.core_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
