ec17bfd0477aac52fdc4460dce071ee9b7aa0438
Mihir <mihirs167@gmail.com>
2026-03-30 23:15:04 +0530

    feat: add react frontend with rider, driver pages, auth context and leaflet maps

--- core-web/src/main.jsx
+++ core-web/src/main.jsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
 import './index.css'
 import App from './App.jsx'
+
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
